13129ced348aeb4b32697f59db42f2deaee729bc
LittleBird <ilms49898723@gmail.com>
2017-03-07 21:50:58 +0900

    unit tests for datachecker and datarecord

---
+++ test/com/github/taffy128s/tlcdbms/DataCheckerTest.java
@@ -0,0 +1,79 @@
+package com.github.taffy128s.tlcdbms;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * DataChecker JUnit Test
+ */
+public class DataCheckerTest {
+    private ArrayList<String> testSet;
+
+    @Before
+    public void setUp() throws Exception {
+        testSet = new ArrayList<>();
+        testSet.add("111");
+        testSet.add("John");
+        testSet.add("123456789123");
+        testSet.add("2147483647");
+        testSet.add("2147483648");
+        testSet.add("21474836472123123123123123");
+        testSet.add("214748364712323232323232323");
+        testSet.add("-2147483648");
+        testSet.add("-2147483648111");
+        testSet.add("-214748364811111111111111111");
+        testSet.add("123sss456");
+        testSet.add("123123123123123123123123123123123123123123123123123123");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void isValidInteger() throws Exception {
+        ArrayList<Boolean> excepted = new ArrayList<>();
+        excepted.add(true);
+        excepted.add(false);
+        excepted.add(false);
+        excepted.add(true);
+        excepted.add(false);
+        excepted.add(false);
+        excepted.add(false);
+        excepted.add(true);
+        excepted.add(false);
+        excepted.add(false);
+        excepted.add(false);
+        excepted.add(false);
+        for (int i = 0; i < excepted.size(); ++i) {
+            assertEquals(excepted.get(i), DataChecker.isValidInteger(testSet.get(i)));
+        }
+    }
+
+    @Test
+    public void isValidVarChar() throws Exception {
+        ArrayList<Boolean> excepted = new ArrayList<>();
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(true);
+        excepted.add(false);
+        for (int i = 0; i < excepted.size(); ++i) {
+            assertEquals(excepted.get(i), DataChecker.isValidVarChar(testSet.get(i)));
+        }
+    }
+
+}
---
+++ test/com/github/taffy128s/tlcdbms/DataRecordTest.java
@@ -0,0 +1,68 @@
+package com.github.taffy128s.tlcdbms;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * DataRecord JUnit Test
+ */
+public class DataRecordTest {
+    private DataRecord dataRecord;
+
+    @Before
+    public void setUp() throws Exception {
+        dataRecord = new DataRecord();
+        dataRecord.append("Bird");
+        dataRecord.append("Male");
+        dataRecord.append(20);
+        dataRecord.append("Computer Science");
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void append() throws Exception {
+        // nothing
+    }
+
+    @Test
+    public void update() throws Exception {
+        dataRecord.update(2, 22);
+        assertEquals(22, dataRecord.get(2));
+        dataRecord.update(2, 20);
+    }
+
+    @Test
+    public void set() throws Exception {
+        dataRecord.set(2, 22);
+        assertEquals(22, dataRecord.get(2));
+        dataRecord.set(2, 20);
+    }
+
+    @Test
+    public void get() throws Exception {
+        // nothing
+    }
+
+    @Test
+    public void getAllFields() throws Exception {
+        Object[] datas = dataRecord.getAllFields();
+        assertEquals(4, datas.length);
+        assertEquals("Bird", datas[0]);
+        assertEquals("Male", datas[1]);
+        assertEquals(20, datas[2]);
+        assertEquals("Computer Science", datas[3]);
+    }
+
+    @Test
+    public void length() throws Exception {
+        assertEquals(4, dataRecord.length());
+    }
+}
