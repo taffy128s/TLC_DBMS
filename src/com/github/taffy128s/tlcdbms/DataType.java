208f33e921fe33f092d2d81704dcbede59a2a1c8
LittleBird <ilms49898723@gmail.com>
2017-03-07 17:33:05 +0900

    data type enum class

---
+++ src/com/github/taffy128s/tlcdbms/DataType.java
@@ -0,0 +1,11 @@
+package com.github.taffy128s.tlcdbms;
+
+/**
+ * All Data types.
+ *
+ * INT: 4-bytes 2's complement integer
+ * VARCHAR: string with length up to 40
+ */
+public enum DataType {
+    INT, VARCHAR
+}
