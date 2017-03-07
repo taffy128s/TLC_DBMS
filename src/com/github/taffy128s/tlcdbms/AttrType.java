729ce835ed8649b84300adb35aabcae264565767
Taffy Cheng <taffy128s@gmail.com>
2017-03-07 19:46:25 +0800

    merge

--- src/com/github/taffy128s/tlcdbms/AttrType.java
+++
@@ -1,6 +0,0 @@
-package com.github.taffy128s.tlcdbms;
-
-public enum AttrType {
-    integer,
-    varchar
-}
--- src/com/github/taffy128s/tlcdbms/Block.java
+++
@@ -1,6 +0,0 @@
-package com.github.taffy128s.tlcdbms;
-
-public class Block {
-    public String varchar;
-    public int integer;
-}
