928649a5b4904ae2a3cbb9405ab2cd389b00f602
Taffy Cheng <taffy128s@gmail.com>
2017-03-08 18:54:05 +0800

    Start getting input.

--- src/com/github/taffy128s/tlcdbms/Main.java
+++ src/com/github/taffy128s/tlcdbms/Main.java
@@ -1,9 +1,32 @@
 package com.github.taffy128s.tlcdbms;
 
-public class Main {
+import java.io.IOException;
+import java.io.InputStreamReader;    
+import java.io.Reader;
 
-    public static void main(String[] args) {
-        
+import com.github.taffy128s.tlcdbms.sqlparsers.ParseResult;
+import com.github.taffy128s.tlcdbms.sqlparsers.SQLParser;    
+    
+public class Main {    
+
+    public static void main(String[] args) throws IOException {
+        String singleIns = "";
+        SQLParser parser = new SQLParser();
+        Reader reader = new InputStreamReader(System.in);
+        int temp;
+        while ((temp = reader.read()) != -1) {
+            char c = (char) temp;
+            if (c != ';') singleIns += c;
+            else {
+                if (!singleIns.equals("") && !singleIns.equals("\n") && !singleIns.equals("\r\n")) {
+                    ParseResult result = parser.parse(singleIns);
+                    if (result != null) {
+                        System.out.print(result.toString());
+                    }
+                }
+                singleIns = "";
+            }
+        }
     }
-
+    
 }
